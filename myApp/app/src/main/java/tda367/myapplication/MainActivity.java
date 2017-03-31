11bc368007bdfec548abd6ef8efb746d670973db
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-03-31 19:51:54 +0200

    Made the main acticity view with a navigation drawer which includes everything that you should be able to access from there

---
+++ myApp/app/src/main/java/tda367/myapplication/MainActivity.java
@@ -0,0 +1,129 @@
+package tda367.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.constraintlayout_for_fragment, new PlayFragment()).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_play) {
+            PlayFragment playFragment = new PlayFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    playFragment,
+                    playFragment.getTag()).commit();
+        } else if (id == R.id.nav_signin) {
+            SignInFragment signInFragment = new SignInFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    signInFragment,
+                    signInFragment.getTag()).commit();
+
+        } else if (id == R.id.nav_achievements) {
+            AcheivementsFragment acheivementsFragment = new AcheivementsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    acheivementsFragment,
+                    acheivementsFragment.getTag()).commit();
+        } else if (id == R.id.nav_statistics) {
+            StatisticsFragment statisticsFragment = new StatisticsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    statisticsFragment,
+                    statisticsFragment.getTag()).commit();
+        } else if (id == R.id.nav_readmore) {
+            ReadMoreFragment readMoreFragment = new ReadMoreFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    readMoreFragment,
+                    readMoreFragment.getTag()).commit();
+        } else if (id == R.id.nav_settings) {
+            SettingsFragment settingsFragment = new SettingsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    settingsFragment,
+                    settingsFragment.getTag()).commit();
+        } else if (id == R.id.nav_aboutappen) {
+            AboutAppenFragment aboutAppenFragment = new AboutAppenFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.constraintlayout_for_fragment,
+                    aboutAppenFragment,
+                    aboutAppenFragment.getTag()).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
