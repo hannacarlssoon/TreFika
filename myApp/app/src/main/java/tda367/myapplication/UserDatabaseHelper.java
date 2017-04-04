d439ade3893928c2fa2b0b269379452295555120
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-04-04 12:46:33 +0200

    Made the methods to create a user table

--- myApp/app/src/main/java/tda367/myapplication/UserDatabaseHelper.java
+++ myApp/app/src/main/java/tda367/myapplication/UserDatabaseHelper.java
@@ -1,5 +1,6 @@
 package tda367.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,9 +19,11 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_USERS = "user";
 
     //User table columns
-    public static final String KEY_USER_ID = "id";
-    public static final String KEY_USER_NAME = "userName";
-    public static final String KEY_USER_PROFIL_PICTURE_URL = "profilPictureUrl";
+    public static final String USER_ID = "_id";
+    public static final String USER_NAME = "userName";
+    public static final String USER_PASSWORD = "userPassword";
+    public static final String USER_EMAIL = "userEmail";
+    public static final String USER_PROFIL_PICTURE_URL = "profilePictureUrl";
 
     public UserDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,9 +33,11 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         String query_user_table = "CREATE TABLE " + TABLE_USERS +
-                "(" + KEY_USER_ID + " INTEGER PRIMARY KEY," +
-                KEY_USER_NAME + " TEXT," +
-                KEY_USER_PROFIL_PICTURE_URL + " TEXT" +
+                "(" + USER_ID + " INTEGER PRIMARY KEY," +
+                USER_NAME + " TEXT," +
+                USER_PASSWORD + " TEXT," +
+                USER_EMAIL + " TEXT, " +
+                USER_PROFIL_PICTURE_URL + " TEXT" +
                 ")";
 
         sqLiteDatabase.execSQL(query_user_table);
@@ -44,5 +49,26 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
             sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
             onCreate(sqLiteDatabase);
         }
+    }
+
+    public void addUser(String userName, String userPassword, String profilePictureUrl) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(USER_NAME, userName);
+        values.put(USER_PASSWORD, userPassword);
+        values.put(USER_PROFIL_PICTURE_URL, profilePictureUrl);
+        db.insert(TABLE_USERS, null, values);
+    }
+
+    public Integer deleteUser(Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_USERS, USER_ID + " = ? ", new String[] {Integer.toString(id)});
+    }
+
+    public void updatePassword(Integer id, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(USER_PASSWORD, password);
+       // db.update(TABLE_USERS, values)
     }
 }
