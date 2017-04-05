f05f87a9d515b8d6fb42fe76699f525d22b04245
Tuubaas <tobli@student.chalmers.se>
2017-04-05 13:59:40 +0200

    resolved merge issues

--- myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
+++ myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
@@ -10,7 +10,7 @@ public class ActivityInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
