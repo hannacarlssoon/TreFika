a1e21dc18ab91a1f87cb4062af98b7689e790cf4
MadeleineLexen <lexen@student.chalmers.se>
2017-04-05 13:30:09 +0200

    small tweek in ActivityInfo

--- myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
+++ myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
@@ -1,5 +1,6 @@
 package tda367.myapplication;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class ActivityInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
