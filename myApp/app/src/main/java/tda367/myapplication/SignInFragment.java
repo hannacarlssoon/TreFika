0006889b38958751d3d8880ec4849f68a9b23c83
Sara <kisara@student.chalmers.se>
2017-04-05 15:04:39 +0200

    Facebook login code, doesn't work yet

--- myApp/app/src/main/java/tda367/myapplication/SignInFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/SignInFragment.java
@@ -3,18 +3,25 @@ package tda367.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookAuthorizationException;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
 
 import static tda367.myapplication.R.id.view;
 
@@ -23,13 +30,14 @@ import static tda367.myapplication.R.id.view;
  * A simple {@link Fragment} subclass.
  */
 public class SignInFragment extends Fragment {
-        LoginButton login_button;
-        CallbackManager callbackManager;
+       LoginButton login_button;
+    private CallbackManager callbackManager;
 
 
     public SignInFragment() {
         // Required empty public constructor
     }
+
 
 
     @Override
@@ -40,9 +48,11 @@ public class SignInFragment extends Fragment {
       initializeControls(view);
         loginWithFB();
         // Inflate the layout for this fragment
-        // ------------------------------------------> change return to view if this works!
+        // ------------------------------------------> change return to return view if this works!
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
+
+
 
     //Initializing facebook controls
     private void initializeControls(View view){
@@ -51,6 +61,7 @@ public class SignInFragment extends Fragment {
         login_button.setReadPermissions("email");
         login_button.setFragment(this);
     }
+
 
     private void loginWithFB(){
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -70,12 +81,16 @@ public class SignInFragment extends Fragment {
             }
         });
     }
-/*
+
+
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+       // Fragment fragment = getFragmentManager().findFragmentById(R.id.nav_signin);
+       // fragment.onActivityResult(requestCode, resultCode, data);
     }
-    */
+
 
 }
