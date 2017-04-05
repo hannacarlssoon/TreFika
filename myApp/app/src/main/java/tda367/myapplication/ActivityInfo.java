0993d89a9355c02f65c1368aca02828422ed929a
MadeleineLexen <lexen@student.chalmers.se>
2017-04-05 15:21:29 +0200

    connected multiquestion, but the restraints aren't working

--- myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
+++ myApp/app/src/main/java/tda367/myapplication/ActivityInfo.java
@@ -1,8 +1,11 @@
 package tda367.myapplication;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityInfo extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class ActivityInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button btn = (Button)findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ActivityInfo.this, QuestionMultiChoice.class));
+            }
+        });
+
     }
+
 
 }
