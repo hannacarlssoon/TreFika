0fe6c18596eb731651df7ec718419ce25e183b8b
Sara <kisara@student.chalmers.se>
2017-04-06 13:54:47 +0200

    Uploaded RAD and meeting agendas

--- myApp/app/src/main/java/tda367/myapplication/PassedLevel.java
+++ myApp/app/src/main/java/tda367/myapplication/PassedLevel.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class PassedLevel extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class PassedLevel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_passed_level);
 
         Button bbtn = (Button)findViewById(R.id.backButton);
