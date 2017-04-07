5d9a196f8b3796e9b67f7aba99deb17d44ba0941
MadeleineLexen <lexen@student.chalmers.se>
2017-04-07 11:52:49 +0200

    started working on making drag and drop work, not finished and it isn't working

---
+++ myApp/app/src/main/java/tda367/myapplication/Model/DragnDrop.java
@@ -0,0 +1,19 @@
+package tda367.myapplication.Model;
+
+import tda367.myapplication.R;
+
+/**
+ * Created by madeleine on 2017-04-07.
+ */
+
+public class DragnDrop extends Query {
+
+    /*findViewByID(R.id.button5).setOnLongClickListener(listenClick);
+    findViewById(R.id.button5).setOnDragListener(listenDrag);
+
+*/
+    @Override
+    public boolean checkAnswer(String answer) {
+        return false;
+    }
+}
