1713b10867423a1ae7775db7925e8f0df0a47bd8
MadeleineLexen <lexen@student.chalmers.se>
2017-04-11 13:16:44 +0200

    question is accesible, but app chrases when trying to set adapter to the listView, can't display the alt.

--- myApp/app/src/main/java/tda367/myapplication/Controller/DragNDrop.java
+++ myApp/app/src/main/java/tda367/myapplication/Controller/DragNDrop.java
@@ -18,65 +18,37 @@ public class DragNDrop extends AppCompatActivity {
 
     private ListView listView;
     private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_ndrop);
-        btn = (Button) findViewById(R.id.dragSubmit);
         listView = (ListView) findViewById(R.id.list);
+        btn = (Button) findViewById(R.id.dragSubmit);
 
         // Defined Array values to show in ListView
         ArrayList<String> values = new ArrayList<String>();
         for (int i = 0; i < CodeSegments.rows.length; ++i) {
             values.add(CodeSegments.rows[i]);
         }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_drag_ndrop, R.id.dragTextView, values);
 
         // Assign adapter to ListView
-        listView.setAdapter(adapter);
+        // listView.setAdapter(adapter);
 
-        //sets listener on submitbutton, checks if answer is correct,
-        // changes view to passedLevel if correct, otherwise to FailedLevel.
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //setAnswer();
-                // if(checkAnswer()){
-                startActivity(new Intent(DragNDrop.this, PassedLevel.class));
-            /* }
-            else{
-            startActivity(new Intent(QuestionMultiChoice.this, FailedLevel.class));
-            }
-             */
+                Intent intent = new Intent(DragNDrop.this, PassedLevel.class);
+                intent.putExtra("ARG_QUESTION", "strifdnd");
+                startActivity(intent);
             }
         });
-
-        /*
-        // ListView Item Click Listener
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                // ListView Clicked item index
-                int itemPosition     = position;
-
-                // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
-            }
-        });
-
-        listView.setAdapter(adapter);
-    }
-
-
-    //Gets the órder of the elements in the listview, and sets that as answer
-    protected void setAnswer(){
-        //TODO get the order of the elements in the listview and set answer to that.
-        //System.out.println(answer);
-    }
-
-*/
 
     }
 }
+
+
+
