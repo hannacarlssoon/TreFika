8d20980a37fd683db3c06c572b6d4d1e6bd3cfb9
MadeleineLexen <lexen@student.chalmers.se>
2017-04-11 15:52:45 +0200

    Havn't implemented drag n drop, but the question is viewable

--- myApp/app/src/main/java/tda367/myapplication/Controller/DragNDrop.java
+++ myApp/app/src/main/java/tda367/myapplication/Controller/DragNDrop.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -37,7 +36,6 @@ public class DragNDrop extends AppCompatActivity {
 
         // Assign adapter to ListView
         listView.setAdapter(adapter);
-
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
