34e08c33d88326f2888242dc7cf5c53b29dfe43d
MadeleineLexen <lexen@student.chalmers.se>
2017-04-18 16:25:00 +0200

    added a file

---
+++ myApp/app/src/main/java/tda367/myapplication/Model/CodeSegments.java
@@ -0,0 +1,10 @@
+package tda367.myapplication.Model;
+
+/**
+ * Created by madeleine on 2017-04-11.
+ */
+
+public class CodeSegments {
+
+    public static final String[] rows = {"Rad 1", "Rad 2", "Rad 3", "Rad 4"};
+}
