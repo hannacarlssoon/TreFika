585eaf4dcdfc0852ff8fb2b9496b60a3ddf8fd79
Tuubaas <tobli@student.chalmers.se>
2017-04-25 22:30:22 +0200

    Now able to set question text

--- myApp/app/src/main/java/tda367/myapplication/Controller/QuestionMultiChoice.java
+++ myApp/app/src/main/java/tda367/myapplication/Controller/QuestionMultiChoice.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import tda367.myapplication.Model.LevelModel;
 import tda367.myapplication.R;
 
 public class QuestionMultiChoice extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class QuestionMultiChoice extends AppCompatActivity {
     private RadioButton radioAnswerButton;
     private Button submitButton;
     private String answer;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,9 @@ public class QuestionMultiChoice extends AppCompatActivity {
         setContentView(R.layout.activity_question_multi_choice);
         radioAnswerGroup = (RadioGroup)findViewById(R.id.radioGroup);
         Button btn = (Button)findViewById(R.id.SubmitButton);
+        textView = (TextView)findViewById(R.id.questionBox);
+
+        setQuestionText();
 
         System.out.println(getIntent().getStringExtra("ARG_QUESTION"));
 
@@ -51,6 +57,12 @@ public class QuestionMultiChoice extends AppCompatActivity {
         radioAnswerButton = (RadioButton)findViewById(selectedId);
         answer = (String) radioAnswerButton.getText();
         System.out.println(answer);
+    }
+
+    private void setQuestionText(){
+        LevelModel lm = LevelActivity.getItemFromList(0);
+        String infoText = lm.getQuestion();
+        textView.setText(infoText);
     }
 
 
