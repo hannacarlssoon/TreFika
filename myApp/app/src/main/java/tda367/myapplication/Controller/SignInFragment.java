8275fa211d6fc4021b121b94c32d707de1e1164d
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-05-02 13:40:45 +0200

    Added a new signUp activity

--- myApp/app/src/main/java/tda367/myapplication/Controller/SignInFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/Controller/SignInFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -23,6 +25,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.share.Sharer;
 
+import tda367.myapplication.Model.User;
 import tda367.myapplication.R;
 
 import static tda367.myapplication.R.id.view;
@@ -31,9 +34,15 @@ import static tda367.myapplication.R.id.view;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SignInFragment extends Fragment {
-       LoginButton login_button;
-    private CallbackManager callbackManager;
+public class SignInFragment extends Fragment implements View.OnClickListener {
+       //LoginButton login_button;
+    //private CallbackManager callbackManager;
+
+    private Button signIn;
+    private Button signUp;
+    private EditText userName;
+    private EditText password;
+    private User user;
 
 
     public SignInFragment() {
@@ -46,26 +55,50 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
-       FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
-      initializeControls(view);
-        loginWithFB();
+       //FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
+      //initializeControls(view);
+        //loginWithFB();
         // Inflate the layout for this fragment
         // ------------------------------------------> change return to return view if this works!
+
+        signIn = (Button) view.findViewById(R.id.signIn);
+        signUp = (Button) view.findViewById(R.id.signUp);
+        userName = (EditText) view.findViewById(R.id.userName);
+        password = (EditText) view.findViewById(R.id.Password);
+
+        signIn.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+
+
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.signIn:
+                if (userName.getText() != null && password.getText() != null) {
+                    user.logIn(userName.getText().toString(), password.getText().toString());
+                }
+                break;
+            case R.id.signUp:
+                Intent intent = new Intent(getActivity(), SignUp.class);
+                startActivity(intent);
+                break;
+        }
+    }
 
 
     //Initializing facebook controls
-    private void initializeControls(View view){
+    /*private void initializeControls(View view){
         callbackManager = CallbackManager.Factory.create();
         login_button = (LoginButton) view.findViewById(R.id.login_button);
         login_button.setReadPermissions("email");
         login_button.setFragment(this);
-    }
+    }*/
 
 
-    private void loginWithFB(){
+    /*private void loginWithFB(){
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -82,17 +115,17 @@ public class SignInFragment extends Fragment {
 
             }
         });
-    }
+    }*/
 
 
 
-    @Override
+   /* @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
        // Fragment fragment = getFragmentManager().findFragmentById(R.id.nav_signin);
        // fragment.onActivityResult(requestCode, resultCode, data);
-    }
+    }*/
 
 
 }
---
+++ myApp/app/src/main/java/tda367/myapplication/Controller/SignUp.java
@@ -0,0 +1,15 @@
+package tda367.myapplication.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tda367.myapplication.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
