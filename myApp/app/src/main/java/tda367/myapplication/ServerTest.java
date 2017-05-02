fd90ef80344d9a9fbc6c5b7cee241721784a9030
Kitzing <kisara@student.chalmers.se>
2017-05-02 17:46:57 +0200

    Added ServerTest-class

---
+++ myApp/app/src/main/java/tda367/myapplication/ServerTest.java
@@ -0,0 +1,13 @@
+package tda367.myapplication;
+
+/**
+ * Created by sarakitzing on 2017-05-02.
+ */
+
+public class ServerTest {
+
+    public static void main (String[] args){
+        Server sigge = new Server("public void");
+        sigge.startRunning();
+    }
+}
