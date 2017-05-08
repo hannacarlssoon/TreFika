7f681642a3fe0d2a534605f3026eb7c4f2fd0c88
Kitzing <kisara@student.chalmers.se>
2017-05-08 18:56:02 +0200

    added writeCode files and fixed it's layout

---
+++ myApp/app/src/main/java/tda367/myapplication/Controller/WriteCode.java
@@ -0,0 +1,15 @@
+package tda367.myapplication.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tda367.myapplication.R;
+
+public class WriteCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_code);
+    }
+}
