8700827f79e6bbcbdbba069d685919bc54311f1d
Kitzing <kisara@student.chalmers.se>
2017-05-10 19:56:48 +0200

    fixed problems in checkAnswer method to make the test work

--- myApp/app/src/main/java/tda367/myapplication/Model/WriteCode.java
+++ myApp/app/src/main/java/tda367/myapplication/Model/WriteCode.java
@@ -21,16 +21,8 @@ public class WriteCode {
 
     public boolean checkAnswer(String userAnswer){
         compileCode(userAnswer);
-        System.out.println("Correct: " + answer);
-        System.out.println("compUser: " + compUserAnswer);
-        System.out.println("Error: " + compUserAnswer.equals("Error"));
-        System.out.println("Wrong answer: " + compUserAnswer.equals(answer));
-        if(compUserAnswer.equals("Error")){
-            return false;
-        } else if (compUserAnswer.equals(answer)){
-            return false;
-        }
-        return true;
+        return compUserAnswer.equals(answer);
+
     }
 
     public void compileCode(String userCode){
--- myApp/app/src/test/java/tda367/myapplication/WriteCodeTest.java
+++ myApp/app/src/test/java/tda367/myapplication/WriteCodeTest.java
@@ -16,8 +16,10 @@ public class WriteCodeTest {
         WriteCode c = new WriteCode("hej", "1 2 3");
         boolean check = c.checkAnswer("for(int i = 1; i<4; i++){ print(i); }");
         boolean check2 = c.checkAnswer("for(int i = 0; i<4; i++){ print(i); }");
+        boolean check3 = c.checkAnswer("for(int i = 1; i<4; i++{ print(i); }");
         assertEquals(check, true);
         assertEquals(check2, false);
+        assertEquals(check3, false);
 
     }
 
