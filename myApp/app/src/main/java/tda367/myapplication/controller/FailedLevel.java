f9694a9f4a3a81547743dd9b3a16c15aaf1df646
MadeleineLexen <lexen@student.chalmers.se>
2017-05-11 12:16:11 +0200

    the question is corrected correctly,edited a textfile

--- myApp/app/src/main/java/tda367/myapplication/controller/FailedLevel.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/FailedLevel.java
@@ -1,19 +1,27 @@
 package tda367.myapplication.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 import tda367.myapplication.R;
+import tda367.myapplication.model.LearnJava;
+import tda367.myapplication.model.MultiChoice;
 
-public class FailedLevel extends AppCompatActivity {
+public class FailedLevel extends AppCompatActivity implements View.OnClickListener{
+
+    private Button tryAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+       // getSupportActionBar().hide();
         setContentView(R.layout.activity_failed_level);
 
+        tryAgain = (Button) findViewById(R.id.tryAgain);
 
         // Making the window smaller
         DisplayMetrics dm = new DisplayMetrics();
@@ -24,5 +32,18 @@ public class FailedLevel extends AppCompatActivity {
 
         getWindow().setLayout((int)(width*0.8), (int)(height*0.4));
 
+        tryAgain.setOnClickListener(this);
+    }
+
+
+    //This method doesn't work, needs to be worked on
+    @Override
+    public void onClick(View v) {
+        if(LearnJava.getInstance().getQuery().toString().equals("MultiChoice")){
+            startActivity(new Intent(FailedLevel.this, QuestionMultiChoice.class));
+        }
+        else if(LearnJava.getInstance().getQuery().toString().equals("ModelFillBlanks")) {
+            startActivity(new Intent(FailedLevel.this, FillInTheBlanks.class));
+        }
     }
 }
