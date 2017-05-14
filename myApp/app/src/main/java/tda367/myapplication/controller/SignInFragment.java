c1e7bef300673763f63930a4eade2d79a9fff702
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-05-14 16:10:33 +0200

    Added comments in SignInFragment and SignUpActivity

--- myApp/app/src/main/java/tda367/myapplication/controller/SignInFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/SignInFragment.java
@@ -4,7 +4,6 @@ package tda367.myapplication.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,49 +14,38 @@ import android.widget.Toast;
 import tda367.myapplication.model.AccountManager;
 import tda367.myapplication.R;
 
-
 /**
- * A simple {@link Fragment} subclass.
+ * Handles the sign in of the user
  */
+
 public class SignInFragment extends Fragment implements View.OnClickListener {
-       //LoginButton login_button;
-    //private CallbackManager callbackManager;
 
     private Button signIn;
     private Button signUp;
     private EditText userName;
     private EditText password;
 
-
-    public SignInFragment() {
-
-        // Required empty public constructor
-    }
-
-
+    public SignInFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
-       //FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
-      //initializeControls(view);
-        //loginWithFB();
-        // Inflate the layout for this fragment
-        // ------------------------------------------> change return to return view if this works!
 
+        //Assigns each component, that is needed to reach, with an id
         signIn = (Button) view.findViewById(R.id.signIn);
         signUp = (Button) view.findViewById(R.id.signUp);
         userName = (EditText) view.findViewById(R.id.userName);
         password = (EditText) view.findViewById(R.id.Password);
 
+        //Sets listeners to the buttons
         signUp.setOnClickListener(this);
         signIn.setOnClickListener(this);
 
         return view;
     }
 
-
+    //Method needed to be overriden when you have onClickListners, handles each buttons actions when clicked
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -65,7 +53,6 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
                 if (isPasswordCorrect(userName.getText().toString(), password.getText().toString())) {
                     AccountManager.getInstance().logIn(userName.getText().toString(), password.getText().toString());
                     MainActivity.setUserInformation(userName.getText().toString());
-                    System.out.println("holaaa");
                 } else {
                     Toast.makeText(getContext(), "Wrong username or password",
                             Toast.LENGTH_LONG).show();
@@ -78,6 +65,7 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
         }
     }
 
+    //Returns wheather the password is correct or not
     private boolean isPasswordCorrect(String username, String password) {
         try {
             return AccountManager.getInstance().getUsers().get(username).getUserPassword().equals(password);
@@ -86,45 +74,5 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
             return false;
         }
     }
-
-
-    //Initializing facebook controls
-    /*private void initializeControls(View view){
-        callbackManager = CallbackManager.Factory.create();
-        login_button = (LoginButton) view.findViewById(R.id.login_button);
-        login_button.setReadPermissions("email");
-        login_button.setFragment(this);
-    }*/
-
-
-    /*private void loginWithFB(){
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                //Write code on what to happen when login successfully
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
-    }*/
-
-
-
-   /* @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-       // Fragment fragment = getFragmentManager().findFragmentById(R.id.nav_signin);
-       // fragment.onActivityResult(requestCode, resultCode, data);
-    }*/
-
 
 }
--- myApp/app/src/main/java/tda367/myapplication/controller/SignUpActivity.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/SignUpActivity.java
@@ -1,28 +1,28 @@
 package tda367.myapplication.controller;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.FileNotFoundException;
-
 import tda367.myapplication.model.AccountManager;
 import tda367.myapplication.service.ImageHandler;
 import tda367.myapplication.R;
 
+/*
+ * Handles the sign up of a user
+ */
+
 public class SignUpActivity extends AppCompatActivity {
 
+    //All components that are needed to be reached within the class
     private EditText username;
     private EditText password;
-    private ImageButton upload;
+
+    //Variable which decieds how many images that are needed for the profile picture
     private int RESULT_LOAD_IMG = 1;
 
     @Override
@@ -30,11 +30,13 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Finds the id for the components
         username = (EditText) findViewById(R.id.Username);
         password = (EditText) findViewById(R.id.Password);
-        upload = (ImageButton) findViewById(R.id.imageButton);
 
+        ImageButton upload = (ImageButton) findViewById(R.id.imageButton);
 
+        //Sets onClickListner to the upload button, which sends the user to the gallery app
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +49,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         Button signUp = (Button) findViewById(R.id.SignUp);
 
+        //Sets onClickListner to the sign up button, signs in if username is okay, else makes toast
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,23 +65,24 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
+    //Returns wheather the user already exists or not
     private boolean checkIfUserExists(String username) {
         try {
             return AccountManager.getInstance().getUsers().containsKey(username);
         } catch (NullPointerException e) {
             e.printStackTrace();
-            System.out.println("fuuuuuuckkkkk");
             return false;
         }
     }
 
+    //Overridden method from the AppCompatActivity, saves the images from the gallery app
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         try {
             ImageHandler.saveImage(requestCode, resultCode, data, this, username.getText().toString(), getApplicationContext());
         } catch (NullPointerException e) {
-            System.out.println("Whaat");
+            e.printStackTrace();
         }
     }
 }
