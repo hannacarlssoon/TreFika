1e6c5c050dbcd5d18427299c9d2b0e04dcd1163d
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-05-14 16:23:57 +0200

    Added comments for the PlayFragment

--- myApp/app/src/main/java/tda367/myapplication/controller/PlayFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/PlayFragment.java
@@ -15,25 +15,20 @@ import tda367.myapplication.R;
 
 
 /**
+ * Handles the setting of the views when the main category buttons are clicked
  * A simple {@link Fragment} subclass.
  * @author HannaCarlsoon. Revised by Madeleine Lexén and Tobias Lindgren.
  */
 public class PlayFragment extends Fragment implements View.OnClickListener {
+
     public LearnJava learnJava = LearnJava.getInstance();
 
-
-
-    public PlayFragment() {
-        // Required empty public constructor
-    }
+    public PlayFragment() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //Intent intent = new Intent(getActivity(), LevelActivity.class);
-        //startActivity(intent);
 
         View view = inflater.inflate(R.layout.fragment_play, container, false);
 
@@ -41,9 +36,12 @@ public class PlayFragment extends Fragment implements View.OnClickListener {
         HashMapCreator hcreate = new HashMapCreator(getContext());
         learnJava.init(hcreate.getHashMap());
 
+        //Sets id:s to the buttons
         Button b1 = (Button) view.findViewById(R.id.category1);
         Button b2 = (Button) view.findViewById(R.id.category2);
         Button b3 = (Button) view.findViewById(R.id.category3);
+
+        //Sets onCLickListeners to the buttons
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
@@ -51,24 +49,21 @@ public class PlayFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    //Overridden method from onClickListners, sets the view when a button is clicked
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.category1:
                 Intent intent1 = new Intent(getActivity(), LevelActivity.class);
                 startActivity(intent1);
-                learnJava.setCurrentCategory("category1");
-                System.out.println("Hola");
                 break;
             case R.id.category2:
                 Intent intent2 = new Intent(getActivity(), LevelActivity.class);
                 startActivity(intent2);
-                learnJava.setCurrentCategory("category2");
                 break;
             case R.id.category3:
                 Intent intent3 = new Intent(getActivity(), LevelActivity.class);
                 startActivity(intent3);
-                learnJava.setCurrentCategory("category3");
                 break;
         }
     }
