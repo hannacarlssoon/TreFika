7e6c38df12dbf83e39abe51bc5ba35c4f698a6fc
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-05-14 16:19:29 +0200

    Added comments in AccountManager and refratored initInstance

--- myApp/app/src/main/java/tda367/myapplication/model/AccountManager.java
+++ myApp/app/src/main/java/tda367/myapplication/model/AccountManager.java
@@ -2,10 +2,8 @@ package tda367.myapplication.model;
 
 import android.content.Context;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,58 +13,41 @@ import tda367.myapplication.service.UserFileReader;
 
 /**
  * Created by hannacarlsson on 2017-05-03.
+ * Handles the log in and add new users methods
  */
 
 public class AccountManager implements Serializable {
+
     private static AccountManager instance;
-
     private Map<String, User> users = new HashMap<>();
-
     private User activeUser;
 
+    //Needed when implementing the Serilizable interface
     private static final long serialVersionUID =7116880369697727491L;
 
-    private AccountManager() {
-    }
+    private AccountManager() {}
 
     public static AccountManager getInstance() {
-        System.out.println("In getInstance");
-        System.out.println(instance.equals(null));
-        System.out.println(instance.getUsers().equals(null));
         return instance;
     }
 
+    //Initialises the AccountManager, loads it from file, only called when App starts
     public static void initInstance(UserFileReader userFileReader, Context context) {
-        System.out.println("Almost there");
         if (instance == null) {
             try {
-                System.out.println("In here yoo");
                 instance = (AccountManager) userFileReader.loadObject(context).readObject();
-            } catch (FileNotFoundException e) {
-                System.out.println("oh no 1");
-                instance = new AccountManager();
-            } catch (ClassNotFoundException e) {
-                System.out.println("Oh no 2");
-                instance = new AccountManager();
+            } catch (Exception e) {
                 e.printStackTrace();
-            }catch (IOException e) {
-                System.out.println("oh no 3");
                 instance = new AccountManager();
-                e.printStackTrace();
             }
         }
     }
 
     //Adds a new user
     public void addUser(String userName, String userPassword) {
-        System.out.println("In addUser");
         if (!users.containsKey(userName)) {
-            System.out.println("In if");
             users.put(userName, new User(userName, userPassword));
             logIn(userName, userPassword);
-        }
-        else {
-            System.out.println("Username already exists");
         }
     }
 
@@ -75,18 +56,17 @@ public class AccountManager implements Serializable {
          try {
              activeUser = users.get(userName);
              activeUser.setProfilePicture(ImageHandler.loadImage(userName));
-             System.out.println("You logged in");
          } catch(NullPointerException e) {
-             System.out.println("User doesn't exist");
             e.printStackTrace();
-        }
-
+         }
     }
 
+    //Returns the active user
     public User getActiveUser() {
         return activeUser;
     }
 
+    //Returns the users hashmap
     public Map<String, User> getUsers() {
         return users;
     }
