561410368a3bbbff48999eb71eaab4366e8c0ee6
MadeleineLexen <lexen@student.chalmers.se>
2017-05-16 14:48:01 +0200

    created a test for multichoice

--- myApp/app/src/test/java/tda367/myapplication/MultiChoiceTest.java
+++ myApp/app/src/test/java/tda367/myapplication/MultiChoiceTest.java
@@ -1,8 +1,22 @@
 package tda367.myapplication;
 
+import org.junit.Test;
+import tda367.myapplication.model.MultiChoice;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by madeleine on 2017-05-13.
  */
 
 public class MultiChoiceTest {
+
+    @Test
+    public void testCheckAnswer(){
+        MultiChoice mc = new MultiChoice("fråga", "svar", "alternativ");
+        boolean check1 = mc.checkAnswer("svar");
+        boolean check2 = mc.checkAnswer("felsvar");
+        assertEquals(check1, true);
+        assertEquals(check2, false);
+    }
 }
