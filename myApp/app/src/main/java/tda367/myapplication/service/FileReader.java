5d50cfd756dbb246d3c4b19247543f8f46f70be0
Tuubaas <tobli@student.chalmers.se>
2017-05-18 11:15:42 +0200

    Changed read file method from reading unneccessary rows. Also changed .txt files

--- myApp/app/src/main/java/tda367/myapplication/service/FileReader.java
+++ myApp/app/src/main/java/tda367/myapplication/service/FileReader.java
@@ -8,11 +8,12 @@ import java.io.InputStreamReader;
 public class FileReader {
 
     private int counter;
-    String stringWRows;
+    private String stringWRows;
+    private String string;
 
     // Method for getting the required string. Returns the question, the answer or the info of the level
     public String getRequiredText(String fileName, String type, Context context){
-        String string = "";
+        string = "";
         counter = 0;
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getAssets().open(fileName)));
@@ -20,31 +21,16 @@ public class FileReader {
             while (lineOfContent != null){
                 counter++;
                 if (checkIfQuestion(type)){
-                    if (lineOfContent.contains("row")){
-                        string = getStringWRows(lineOfContent);
-                    }
-                    else {
-                        string = lineOfContent;
-                    }
+                    rowCheck(lineOfContent);
                 }
                 else if (checkIfAnswer(type)) {
                     string = lineOfContent;
                 }
                 else if (checkIfInfo(type)) {
-                    if (lineOfContent.contains("row")) {
-                        string = getStringWRows(lineOfContent);
-                    }
-                    else {
-                        string = lineOfContent;
-                    }
+                    rowCheck(lineOfContent);
                 }
                 else if (checkIfHint(type)) {
-                    if (lineOfContent.contains("row")) {
-                        string = getStringWRows(lineOfContent);
-                    }
-                    else {
-                        string = lineOfContent;
-                    }
+                    rowCheck(lineOfContent);
                 }
                 else if (checkIfAlternatives(type)) {
                     string = lineOfContent;
@@ -64,32 +50,32 @@ public class FileReader {
 
     // Method for checking if the wanted text is a question and the line of the .txt-file is a question
     private boolean checkIfQuestion(String reqText){
-        return (reqText.equals("question") && counter == 2);
+        return (reqText.equals("question") && counter == 1);
     }
 
     // Method for checking if the wanted text is an answer and the line of the .txt-file is an answer
     private boolean checkIfAnswer(String reqText){
-        return (reqText.equals("answer") && counter == 4);
+        return (reqText.equals("answer") && counter == 2);
     }
 
     // Method for checking if the wanted text is info and the line of the .txt-file is info
     private boolean checkIfInfo(String reqText){
-        return (reqText.equals("info") && counter == 6);
+        return (reqText.equals("info") && counter == 3);
     }
 
     //Method for checking if the wanted text is a hint and the line of the .txt-file is hint
     private boolean checkIfHint(String reqText){
-        return (reqText.equals("hint") && counter == 8);
+        return (reqText.equals("hint") && counter == 4);
     }
 
     //Method for checking if the wanted text is alternatives and the line of the .txt-file is alternatives
     private boolean checkIfAlternatives(String reqText){
-        return (reqText.equals("alternative") && counter == 10);
+        return (reqText.equals("alternative") && counter == 5);
     }
 
     //Method for checking if the wanted text is a heading and the line of the .txt-file is heading
     private boolean checkIfHeading(String reqText){
-        return (reqText.equals("heading") && counter == 12);
+        return (reqText.equals("heading") && counter == 6);
     }
 
     //Returns The same string but with newlines
@@ -104,6 +90,16 @@ public class FileReader {
         String[] splitString = fileString.split("row");
         for (String string:splitString){
             stringWRows = stringWRows + string + "\n";
+        }
+    }
+
+    //Method for checking if line has "row"
+    private void rowCheck (String lineOfContent) {
+        if (lineOfContent.contains("row")){
+            string = getStringWRows(lineOfContent);
+        }
+        else {
+            string = lineOfContent;
         }
     }
 }
