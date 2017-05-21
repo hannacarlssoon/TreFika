7d70a3fb405c8f5d77fc8cb81d76102d658d9c50
Tuubaas <tobli@student.chalmers.se>
2017-05-21 17:41:27 +0200

    Added missing files

---
+++ myApp/app/src/main/java/tda367/myapplication/DetailInfo.java
@@ -0,0 +1,18 @@
+package tda367.myapplication;
+
+/**
+ * Created by Tubas on 2017-05-21.
+ */
+
+public class DetailInfo {
+
+    private String name = "";
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
