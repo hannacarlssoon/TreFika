5aebc05ab959bdf61c9b6405cd713d39512bb18c
Tuubaas <tobli@student.chalmers.se>
2017-05-21 17:36:27 +0200

    Added an expandablelistview to read more fragment to fit all texts

--- myApp/app/src/main/java/tda367/myapplication/controller/ReadMoreFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/ReadMoreFragment.java
@@ -1,31 +1,36 @@
 package tda367.myapplication.controller;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import tda367.myapplication.DetailInfo;
+import tda367.myapplication.HeaderInfo;
 import tda367.myapplication.R;
+import tda367.myapplication.ReadMoreListAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReadMoreFragment extends Fragment {
-    private TextView textTyp;
-    private TextView textIf;
-    private TextView textFor;
-    private TextView textArr;
-    private String typ = "Något om typer här";
-    private String ifbo = "Något om if/boolean här";
-    private String forwhi = "Något om for/while här";
-    private String arr = "Något om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer härNågot om arrayer här";
 
+    private LinkedHashMap<String, HeaderInfo> mySection = new LinkedHashMap<>();
+    private ArrayList<HeaderInfo> SectionList = new ArrayList<>();
 
+    private ReadMoreListAdapter listAdapter;
+    private ExpandableListView expandableListView;
 
     public ReadMoreFragment() {
 
@@ -37,22 +42,83 @@ public class ReadMoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_read_more, container, false);
-        textTyp = (TextView) view.findViewById(R.id.textTyp);
-        textIf = (TextView) view.findViewById(R.id.textIf);
-        textFor = (TextView) view.findViewById(R.id.textFor);
-        textArr = (TextView) view.findViewById(R.id.textArr);
 
-        textTyp.setText(typ);
-        textIf.setText(ifbo);
-        textFor.setText(forwhi);
-        textArr.setText(arr);
-        textTyp.setMovementMethod(new ScrollingMovementMethod());
-        textIf.setMovementMethod(new ScrollingMovementMethod());
-        textFor.setMovementMethod(new ScrollingMovementMethod());
-        textArr.setMovementMethod(new ScrollingMovementMethod());
+        //Just add some data to start with
+        addProducts();
+        //get reference to the ExpandableListView
+        expandableListView = (ExpandableListView) view.findViewById(R.id.expandable_list);
+        //create the adapter by passing your ArrayList data
+        listAdapter = new ReadMoreListAdapter(view.getContext(), SectionList);
+        //attach the adapter to the list
+        expandableListView.setAdapter(listAdapter);
+
+        //expand all Groups
+        collapseAll();
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    //method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            expandableListView.expandGroup(i);
+        }
+    }
+
+    //method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            expandableListView.collapseGroup(i);
+        }
+    }
+
+    //load some initial data into out list
+    private void addProducts(){
+
+        addProduct("Typer","I java sparas värden i variabler. Dessa variabler har en deklarerad typ. Det finns 8 primitiva typer, där de vanligaste är boolean, int, double och char. booleans kan hålla två olika värden, true och false. double och int håller båda tal, där double håller decimaltal och int håller heltal. char kan hålla ett tecken. De primitiva typerna skrivs alltid med liten inledande bokstav. Exempel på hur variabler av dessa typer deklareras och instansieras kan ses nedan:\n" +
+                "\n" +
+                "int i = 5;\n" +
+                "char alpha = ‘a’;\n" +
+                "boolean isThisTrue = false;\n" +
+                "double decimals = 2.5;\n" +
+                "\n" +
+                "Det finns också andra typer i java, en väldigt användbar är String. Denna typen håller strängar av text. String skrivs med stor inledande bokstav. En String-variabel kan deklareras som String text;");
+        addProduct("Boolean-logik och If-satser","Cabbage");
+        addProduct("For- och While-loopar","Onion");
+
+        addProduct("Arrayer","Apple");
+    }
+
+    //here we maintain our products in various departments
+    private int addProduct(String department, String product){
+
+        int groupPosition = 0;
+
+        //check the hash map if the group already exists
+        HeaderInfo headerInfo = mySection.get(department);
+        //add the group if doesn't exists
+        if(headerInfo == null){
+            headerInfo = new HeaderInfo();
+            headerInfo.setName(department);
+            mySection.put(department, headerInfo);
+            SectionList.add(headerInfo);
+        }
+
+        //get the children for the group
+        ArrayList<DetailInfo> productList = headerInfo.getProductList();
+
+        //create a new child and add that to the group
+        DetailInfo detailInfo = new DetailInfo();
+        detailInfo.setName(product);
+        productList.add(detailInfo);
+        headerInfo.setProductList(productList);
+
+        //find the group position inside the list
+        groupPosition = SectionList.indexOf(headerInfo);
+        return groupPosition;
     }
 
 }
