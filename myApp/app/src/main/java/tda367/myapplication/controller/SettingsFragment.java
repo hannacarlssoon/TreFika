a7a36839d6560a38994077ea7363e837d0d4388d
Tuubaas <tobli@student.chalmers.se>
2017-05-22 11:07:48 +0200

    Deleted option to change language, should not be an option

--- myApp/app/src/main/java/tda367/myapplication/controller/SettingsFragment.java
+++ myApp/app/src/main/java/tda367/myapplication/controller/SettingsFragment.java
@@ -2,11 +2,11 @@ package tda367.myapplication.controller;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +15,7 @@ import android.widget.ImageButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.Locale;
 
@@ -27,10 +28,6 @@ import tda367.myapplication.R;
  */
 public class SettingsFragment extends Fragment {
 
-    private ImageButton sweBtn;
-    private ImageButton engBtn;
-    private RadioGroup langGroup;
-    private String lang;
     private SeekBar volumeSeekBar;
     private TextView volumeText;
     private ImageButton muteButton;
@@ -49,12 +46,6 @@ public class SettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         context = view.getContext();
-
-        sweBtn = (ImageButton)view.findViewById(R.id.se);
-        engBtn = (ImageButton)view.findViewById(R.id.en);
-        langGroup = new RadioGroup(context);
-        sweBtn.setOnClickListener(langListener);
-        engBtn.setOnClickListener(langListener);
 
         volumeSeekBar = (SeekBar)view.findViewById(R.id.volumeSeekBar);
         volumeText = (TextView)view.findViewById(R.id.volumeText);
@@ -137,22 +128,4 @@ public class SettingsFragment extends Fragment {
         setUnMutePicture();
     }
 
-    private ImageButton.OnClickListener langListener = new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    lang = sweBtn.getId() + "";
-                    setLocale(lang);
-                }
-            };
-
-    private void setLocale(String lang){
-        Locale locale = new Locale(lang);
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-        conf.locale = locale;
-        res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(context, MainActivity.class);
-        startActivity(refresh);
-    }
 }
