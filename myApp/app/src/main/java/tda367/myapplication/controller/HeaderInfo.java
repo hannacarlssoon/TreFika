2383cda2417c2f8b430d913b5b2eab6d3e6df108
Tuubaas <tobli@student.chalmers.se>
2017-05-23 09:48:40 +0200

    Added files

---
+++ myApp/app/src/main/java/tda367/myapplication/controller/HeaderInfo.java
@@ -0,0 +1,30 @@
+package tda367.myapplication.controller;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tobias Lindgren on 2017-05-21.
+ * source: https://www.codeproject.com/Articles/1151814/Android-ExpandablelistView-Tutorial-with-Android-C
+ */
+
+public class HeaderInfo {
+
+    private String name;
+    private ArrayList<DetailInfo> productList = new ArrayList<>();
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public ArrayList<DetailInfo> getProductList(){
+        return productList;
+    }
+
+    public void setProductList(ArrayList<DetailInfo> detailList){
+        this.productList = detailList;
+    }
+}
---
+++ myApp/app/src/main/java/tda367/myapplication/controller/ReadMoreListAdapter.java
@@ -0,0 +1,97 @@
+package tda367.myapplication.controller;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import tda367.myapplication.R;
+
+/**
+ * Created by Tobias Lindgren on 2017-05-21.
+ * source: https://www.codeproject.com/Articles/1151814/Android-ExpandablelistView-Tutorial-with-Android-C
+ */
+
+public class ReadMoreListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<HeaderInfo> deptList;
+
+    public ReadMoreListAdapter(Context context, ArrayList<HeaderInfo> deptList){
+        this.context = context;
+        this.deptList = deptList;
+    }
+    @Override
+    public int getGroupCount() {
+        return deptList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        ArrayList<DetailInfo> productList =
+                deptList.get(groupPosition).getProductList();
+        return productList.size();    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return deptList.get(groupPosition);    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        ArrayList<DetailInfo> productList = deptList.get(groupPosition).getProductList();
+        return productList.get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        HeaderInfo headerInfo = (HeaderInfo) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater inf = (LayoutInflater)
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inf.inflate(R.layout.group_heading, null);
+        }
+
+        TextView heading = (TextView) convertView.findViewById(R.id.group_heading);
+        heading.setText(headerInfo.getName().trim());
+
+        return convertView;    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        DetailInfo detailInfo = (DetailInfo) getChild(groupPosition, childPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater)
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.child_row, null);
+        }
+
+        TextView childItem = (TextView) convertView.findViewById(R.id.child_row);
+        childItem.setText(detailInfo.getName().trim());
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
