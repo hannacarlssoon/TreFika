257f7bf640a0b3ddf5cb1d0cc4e24a3768ba1f75
Tuubaas <tobli@student.chalmers.se>
2017-05-25 16:42:36 +0200

    removed unneccessary methods

--- myApp/app/src/main/java/tda367/myapplication/service/FileReader.java
+++ myApp/app/src/main/java/tda367/myapplication/service/FileReader.java
@@ -22,7 +22,7 @@ public class FileReader {
     private String string;
     List<String> strings;
 
-
+    //Method returns array containing each part of a level
     public List<String> getTextArray(String filename, Context context){
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getAssets().open(filename)));
@@ -37,37 +37,6 @@ public class FileReader {
 
         }
         return strings;
-    }
-
-
-    // Method for checking if the wanted text is a question and the line of the .txt-file is a question
-    private boolean checkIfQuestion(String reqText){
-        return (reqText.equals("question") && counter == 1);
-    }
-
-    // Method for checking if the wanted text is an answer and the line of the .txt-file is an answer
-    private boolean checkIfAnswer(String reqText){
-        return (reqText.equals("answer") && counter == 2);
-    }
-
-    // Method for checking if the wanted text is info and the line of the .txt-file is info
-    private boolean checkIfInfo(String reqText){
-        return (reqText.equals("info") && counter == 3);
-    }
-
-    //Method for checking if the wanted text is a hint and the line of the .txt-file is hint
-    private boolean checkIfHint(String reqText){
-        return (reqText.equals("hint") && counter == 4);
-    }
-
-    //Method for checking if the wanted text is alternatives and the line of the .txt-file is alternatives
-    private boolean checkIfAlternatives(String reqText){
-        return (reqText.equals("alternative") && counter == 5);
-    }
-
-    //Method for checking if the wanted text is a heading and the line of the .txt-file is heading
-    private boolean checkIfHeading(String reqText){
-        return (reqText.equals("heading") && counter == 6);
     }
 
     //Returns The same string but with newlines
